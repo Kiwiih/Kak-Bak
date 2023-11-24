f6f207d8c93b2faed8ba175d7fd3f1133eecf8f9
Moa Sandberg <moisan1999@gmail.com>
2023-11-24 11:52:01 +0100

    updated js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const updatePortions = document.getElementById('update-portions');
+const updatePortionsButtons = document.querySelectorAll('.update-portions-btn');
 const recipeContainer = document.getElementById('recipe-container');
 const vaniljkakaDiv = document.getElementById('vaniljkaka');
 const chokladkakaDiv = document.getElementById('chokladkaka');
 const kardemummabakelseDiv = document.getElementById('kardemummabakelse');
 const blueberrycakeDiv = document.getElementById('blabarstarta');
 
-updatePortions.addEventListener('click', updatePortionsFunc);
+updatePortionsButtons.forEach(button => {
+  button.addEventListener('click', updatePortionsFunc);
+});
 
 const recipes = [
   {
@@ -27,69 +29,80 @@ const recipes = [
       { name: "Vanilj", baseAmount: 2, unit: "msk" }
     ],
     instructions: "Blanda alla ingredienser och grädda i 175 grader i 20 minuter."
+  },
+  {
+    recipeName: "Kardemummabakelse",
+    portions: 1,
+    ingredients: [
+      { name: "Mjöl", baseAmount: 1, unit: "kopp" },
+      { name: "Socker", baseAmount: 0.5, unit: "kopp" },
+      { name: "Kardemumma", baseAmount: 2, unit: "msk" }
+    ], 
+    instructions: "Blanda alla ingredienser och grädda i 175 grader i 20 minuter."
+  },
+  {
+    recipeName: "Blåbärstårta",
+    portions: 1,
+    ingredients: [
+      { name: "Mjöl", baseAmount: 1, unit: "kopp" },
+      { name: "Socker", baseAmount: 0.5, unit: "kopp" },
+      { name: "Blåbär", baseAmount: 2, unit: "msk" }
+    ], 
+    instructions: "Blanda alla ingredienser och grädda i 175 grader i 20 minuter."
   }
   // Add more recipe objects here
 ];
 
-function updatePortionsFunc() {
-  const newPortionCount = parseInt(document.getElementById('portion-count').value);
+function updatePortionsFunc(event) {
+  const button = event.target;
+  const recipeName = button.dataset.recipe;
 
-  recipes.forEach(recipe => {
+  const portionInput = document.getElementById(`portion-count-${recipeName}`);
+  if (!portionInput) {
+    console.error("Portion input not found for recipe:", recipeName);
+    return;
+  }
+
+  const newPortionCount = parseInt(portionInput.value);
+
+  const recipe = recipes.find(recipe => recipe.recipeName === recipeName);
+  if (recipe) {
     const updatedRecipe = calculateUpdatedRecipe(recipe, newPortionCount);
     renderRecipe(updatedRecipe);
-  });
-}
-
-function calculateUpdatedRecipe(recipe, newPortionCount) {
-  const updatedIngredients = recipe.ingredients.map(ingredient => ({
-    ...ingredient,
-    updatedAmount: ingredient.baseAmount * newPortionCount
-  }));
-
-  return {
-    ...recipe,
-    portions: newPortionCount,
-    ingredients: updatedIngredients
-  };
+  } else {
+    console.error("Recipe not found:", recipeName);
+  }
 }
 
 function renderRecipe(recipe) {
-    if (!recipe || !recipe.recipeName) {
-      console.error("Invalid recipe data:", recipe);
-      return;
-    }
-  
-    let recipeElement = document.getElementById(recipe.recipeName);
-  
-    if (!recipeElement) {
-      recipeElement = document.createElement('div');
-      recipeElement.classList.add('recipes');
-      recipeElement.id = recipe.recipeName.toLowerCase();
-      chokladkakaDiv.appendChild(recipeElement);
-
-  
-      const ingredientList = document.createElement('ul');
-      ingredientList.id = `${recipe.recipeName.toLowerCase()}-ingredients`;
-      recipeElement.appendChild(ingredientList);
-    }
-  
-    // Update portions input
-    const portionInput = document.getElementById(`portion-count-${recipe.recipeName}`);
-    if (portionInput) {
-      portionInput.value = recipe.portions;
-    }
-  
-    // Update ingredient list
-    const ingredientList = document.getElementById(`${recipe.recipeName.toLowerCase()}-ingredients`);
-    if (ingredientList) {
-      ingredientList.innerHTML = ''; // Clear previous content
-  
-      recipe.ingredients.forEach(ingredient => {
-        const ingredientListItem = document.createElement('li');
-        ingredientListItem.textContent = `${ingredient.name}: ${ingredient.updatedAmount} ${ingredient.unit}`;
-        ingredientList.appendChild(ingredientListItem);
-      });
-    } else {
-      console.error("Ingredient list not found for recipe:", recipe);
-    }
+  if (!recipe || !recipe.recipeName) {
+    console.error("Invalid recipe data:", recipe);
+    return;
   }
+
+  let recipeElement = document.getElementById(recipe.recipeName);
+
+  if (!recipeElement) {
+    recipeElement = document.createElement('div');
+    recipeElement.classList.add('recipes');
+    recipeElement.id = recipe.recipeName.toLowerCase();
+    recipeContainer.appendChild(recipeElement);
+
+    const heading = document.createElement('h3');
+    heading.textContent = recipe.recipeName;
+    recipeElement.appendChild(heading);
+
+    // Create portion input
+    const portionInput = document.createElement('input');
+    portionInput.type = 'number';
+    portionInput.id = `portion-count-${recipe.recipeName}`;
+    portionInput.value = recipe.portions;
+    portionInput.min = 1;
+    recipeElement.appendChild(portionInput);
+
+    // Create update button
+    const updateButton = document.createElement('button');
+    updateButton.textContent = 'Uppdatera portioner';
+    updateButton.addEventListener('click', updatePortionsFunc);
+    updateButton.dataset.recipe = recipe.recipeName;
+    recipeElement}}
